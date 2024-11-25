52092d7ba8b99916abe9782e4fc24de6f725730b
GeorgeUrtaev <shpanka1996@gmail.com>
2024-11-26 01:37:02 +0300

    second commit

---
+++ src/components/selector.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Селектор</h1>
+    <select @change="$emit('translateFunc',translatedArr,selectedValue)" v-model="selectedValue">   
+        <option v-for="yazik in translatedArr" :value="yazik.lang">{{yazik.lang}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+    props:
+    {
+      translatedArr: Array,
+    },
+    data()
+    {
+      return {
+        selectedValue: "Ру",
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
